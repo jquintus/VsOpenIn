805212df096204f14942e59cbf8d42c5ae2221e7
jquintus <josh.quintus@live.com>
2015-02-17 12:17:00 -0500

    Clean up options dialog and text a bit.

--- VsOpenIn/VsOpenIn/Options/OptionDialog.cs
+++ VsOpenIn/VsOpenIn/Options/OptionDialog.cs
@@ -12,41 +12,49 @@ namespace MasterDevs.VsOpenIn.Options
     [CLSCompliant(false), ComVisible(true)]
     public class OptionDialog : DialogPage
     {
+        #region Constants
         public const string DEFAULT_PATH = @"C:\Program Files (x86)\vim\vim74\gvim.exe";
-
         /// <summary>
         /// explanation of arguments http://vim.wikia.com/wiki/Integrate_gvim_with_Visual_Studio
         /// </summary>
         public const string DEFAULT_ARGUMENTS = "--servername VimStudio --remote-silent \"+call cursor({CurrentLine}, {CurrentCol})\" \"{FullName}\"";
-        public const string ARGUMENTS_DESCRIPTION = @"Default:
-    --servername VimStudio --remote-silent ""+call cursor({0}, {1})"" ""{2}""" + VARIABLES;
-        public const string DIRECTORY_DESCRIPTION = @"The working directory for the external editor" + VARIABLES;
-        public const string VARIABLES = @"
+        public const string DEFAULT_DIRECTORY = "{DirectoryName}";
 
-Variables
+        public const string DESCRIPTION_ARGUMENTS = "Default:\n    "  + DEFAULT_ARGUMENTS + DESCRIPTION_VARIABLES;
+        public const string DESCRIPTION_DIRECTORY = @"The working directory for the external editor" + DESCRIPTION_VARIABLES;
+        public const string DESCRIPTION_VARIABLES = "\n\n" + @"Variables
     {CurrentLine} is Current Line Number
     {CurrentCol} is Current Column Number
     {FullName} is Full File Path
     {DirectoryName} is File Directory
     {FileName} is File Name";
 
+        #endregion
+
+        public OptionDialog()
+        {
+            EditorPath = DEFAULT_PATH;
+            Arguments = DEFAULT_ARGUMENTS;
+            InitialDirectory = DEFAULT_DIRECTORY;
+        }
+
         [Category("VS Open In")]
-        [DisplayName("Path to external editor")]
+        [DisplayName("Editor Path")]
         [Description("The full path to the external editor")]
         [DefaultValue(DEFAULT_PATH)]
         [Editor(typeof(FileNameEditor), typeof(UITypeEditor))]
-
-        public string OpenPath { get; set; }
+        public string EditorPath { get; set; }
 
         [Category("VS Open In")]
-        [DisplayName("Arguments to pass to the external editor")]
-        [Description(ARGUMENTS_DESCRIPTION)]
+        [DisplayName("Editor Arguments")]
+        [DefaultValue(DEFAULT_ARGUMENTS)]
+        [Description(DESCRIPTION_ARGUMENTS)]
         public string Arguments { get; set; }
 
         [Category("VS Open In")]
         [DisplayName("Initial Directory")]
-        [Description(DIRECTORY_DESCRIPTION)]
-        [DefaultValue("{DirectoryName}")]
+        [Description(DESCRIPTION_DIRECTORY)]
+        [DefaultValue(DEFAULT_DIRECTORY)]
         public string InitialDirectory { get; set; }
     }
 
