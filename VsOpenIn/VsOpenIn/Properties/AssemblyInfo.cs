622a115f3afe7e316d3ca7ce9383092f1cd2d8a1
Josh <josh.quintus@gmail.com>
2015-08-09 14:33:15 -0400

    Up the version number and remove VS versions that can't have extensions.

--- VsOpenIn/VsOpenIn/Properties/AssemblyInfo.cs
+++ VsOpenIn/VsOpenIn/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyFileVersion("1.6.0.0")]
 
 [assembly: InternalsVisibleTo("VsOpenIn_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001d6b82d7cbe2894d2509aa7fef62a40aa7df5699952f957ec0b139c16a5260b26561eca78c6bd0a36e090ea17311f58f49a84ba0ed4bd768d61a833a81c07f9feae23907222c5fb8913567713b15d5e95b9628ed0053a7c74b285e42e66a6c91236bb96d45b69fadf4d107e7aa23890f4b4c9a44656f05c6d02436786458bdde")]
 [assembly: InternalsVisibleTo("VsOpenIn_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001d6b82d7cbe2894d2509aa7fef62a40aa7df5699952f957ec0b139c16a5260b26561eca78c6bd0a36e090ea17311f58f49a84ba0ed4bd768d61a833a81c07f9feae23907222c5fb8913567713b15d5e95b9628ed0053a7c74b285e42e66a6c91236bb96d45b69fadf4d107e7aa23890f4b4c9a44656f05c6d02436786458bdde")]
